1b38b4b6981349fa71a64696f018ebf4963a1005
2022BIT003 <155894603+2022BIT003@users.noreply.github.com>
2024-12-24 00:01:36 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Who is the father of C language?",
+    answer: "Dennis Ritchie",
+    options: [
+      "Steve Jobs",
+      "James Gosling",
+      "Dennis Ritchie",
+      "Rasmus Lerdorf"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Which of the following is not a valid C variable name?",
+    answer: "int $main;",
+    options: [
+      "int number;",
+      "float rate;",
+      "int variable_count;",
+      "int $main;"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which is valid C expression?",
+    answer: "int my_num = 100000;",
+    options: [
+      "int my_num = 100,000;",
+      "int my_num = 100000;",
+      "int my num = 1000;",
+      "int $my_num = 10000;"
+    ]
+  },
+  {
+    numb: 4,
+    question: "How many keywords are there in C language?",
+    answer: "32",
+    options: [
+      "32",
+      "33",
+      "64",
+      "18"
+    ]
+  },
+  {
+    numb: 5,
+    question: "C language is a _.",
+    answer: "All of the above",
+    options: [
+      "Procedural oriented programming language",
+      "General purpose programming language",
+      "Structured programming",
+      "All of the above"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which of the following functions is used to print something in C?",
+    answer: "printf()",
+    options: [
+      "scanf()",
+      "printf()",
+      "getchar()",
+      "putchar()"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What does the 'continue' statement do in a loop?",
+    answer: "Skips the current iteration and continues with the next one",
+    options: [
+      "Exits the loop",
+      "Restarts the loop from the beginning",
+      "Skips the current iteration and continues with the next one",
+      "Ends the program"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which of the following is used to end a statement in C?",
+    answer: "Semicolon (;)",
+    options: [
+      "Colon (:)",
+      "Semicolon (;)",
+      "Comma (,)",
+      "Period (.)"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which header file is essential for using the 'printf()' function?",
+    answer: "stdio.h",
+    options: [
+      "stdlib.h",
+      "stdio.h",
+      "string.h",
+      "math.h"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What is the size of the 'int' data type in C (on a 32-bit system)?",
+    answer: "4 bytes",
+    options: [
+      "1 bytes",
+      "2 bytes",
+      "4 bytes",
+      "8 bytes"
+    ]
+  }
+];
